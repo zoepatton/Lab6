b1493cccb341dfec5f7375440438d0a1725b4e8e
Zoe Patton <zoepa801@student.liu.se>
2020-10-06 08:26:20 +0200

    adding brute_force function and documentation

--- knapsack/R/brute_force.R
+++ knapsack/R/brute_force.R
@@ -1,5 +1,51 @@
-#brute force function
-knapsack_brute_force<-function(x,W){
-  
-}
+#' Largest possible value in weighted bag
+#'
+#' This function takes the values and weights provided in a data frame and finds the largest possible sum of values that is less than or equal to the inputed weight. 
+#'
+#' @param x data frame
+#' @param W positive numeric value
+#' @return The output will be a numerical value(the largest possible sum) and the corresponding numerical elements that make that sum
+#' @export
 
+set.seed(42)
+n <- 2000
+knapsack_objects <-
+  data.frame(
+    w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000)
+  )
+
+#largest value (sum of v's in the data frame) that is below the overall weight 
+
+brute_force_knapsack<-function(x,W){
+  if (!is.data.frame(x)){
+    stop("x is not a data frame")
+  }
+  if (W<0){
+    stop("W needs to be positive")
+  }
+    v<-x$v
+    w<-x$w
+    if (all(v>0)&all(w>0)){
+      n<-nrow(x)
+      name<-rownames(x)
+      
+      elementCombos<-list()
+      weights<-list()
+      values<-list()
+      for (i in 1:n){
+        elementCombos[[i]]<-combn(name,i,paste, collapse = "")
+        weights[[i]]<-combn(w,i, FUN=sum)
+        values[[i]]<-combn(v,i, FUN=sum)
+      }
+      
+      maxweight<-which(unlist(weights)<=W)
+      correspondingValues<-unlist(values)[maxweight]
+      valuesmax<-max(correspondingValues)
+      
+      index<-which(unlist(values) == valuesmax)
+      coorespondingElement<-unlist(elementCombos)[index]
+      return(list(value=valuesmax,elements=unlist(coorespondingElement)))
+    }
+    else
+      stop("values are not positive")
+}
---
+++ knapsack/R/knapsack_package.R
@@ -0,0 +1,8 @@
+#' knapsack: A package for computing the maximum sum of values in a bag with limited weight. 
+#'
+#' The knapsack package provides three functions:
+#' brute_force, dynamic and greedy. 
+#' 
+#'
+#' @docType package
+#' @name knapsack
